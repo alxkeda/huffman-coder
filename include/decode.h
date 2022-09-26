d89ffd8ee8a43f2c9a48ecb46477e3a85ebe0d5d
Alex Ikeda <54652078+alxkeda@users.noreply.github.com>
2022-09-26 09:10:58 -0600

    created binaries

--- include/decode.h
+++ include/decode.h
@@ -1,7 +1,11 @@
-// #pragma once
+#pragma once
 
-// #include "f.h"
+#include <string>
+#include <map>
 
-// std::string decode(std::string sequence) {
-    
-// }
+#include "f.h"
+
+const Node* decode_tree(std::string* sequence, int* i);
+std::map<std::string, char> create_tree(std::string sequence);
+void traverse(std::map<std::string, char>* encoding, const Node* curr, std::string* temp);
+std::string decode(std::string sequence);
--- include/encode.h
+++ include/encode.h
@@ -1,13 +1,11 @@
 #pragma once
 
-#include <iostream>
 #include <string>
 #include <map>
 #include <queue>
-#include <cstring>
 
 #include "f.h"
 
-std::map<const char, std::string> create_encoding(std::string sequence, std::priority_queue<const Node*, std::vector<const Node*>, Node::CompNodeFreq> frequencies);
-void traverse(std::map<const char, std::string>* encoding, const Node* curr, std::string* temp, std::priority_queue<const Node*, std::vector<const Node*>, Node::CompNodeFreq> frequencies);
+std::map<const char, std::string> create_tree(std::string sequence, std::priority_queue<const Node*, std::vector<const Node*>, Node::CompNodeFreq> frequencies);
+void traverse(std::map<const char, std::string>* encoding, const Node* curr, std::string* temp, std::string* tree);
 std::string encode(std::string sequence, std::priority_queue<const Node*, std::vector<const Node*>, Node::CompNodeFreq> frequencies);
