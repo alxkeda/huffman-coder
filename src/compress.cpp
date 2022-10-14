b9f36cce039d0110104b2bdcd9e90d73d2324402
Alex Ikeda <54652078+alxkeda@users.noreply.github.com>
2022-10-14 11:23:29 -0600

    added test cases

--- src/compress.cpp
+++ src/compress.cpp
@@ -2,9 +2,25 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <iomanip>
+#include <processthreadsapi.h>
 
 #include "../include/f.h"
 #include "../include/encode.h"
+
+// const double ENCODING = ;
+// provide the original encoding size above
+
+double retrieve_cpu_time(){
+    FILETIME a, b, c, d;
+    if(GetProcessTimes(GetCurrentProcess(), &a, &b, &c, &d) != 0) {
+        return
+            (double)(d.dwLowDateTime |
+            ((unsigned long long)d.dwHighDateTime << 32)) * 0.0000001;
+    } else {
+        return 1;
+    }
+}
 
 int main() {
 
@@ -15,8 +31,13 @@ int main() {
 
     const std::priority_queue<const Node*, std::vector<const Node*>, Node::CompNodeFreq> frequencies = Metadata::collect_freq(input);
 
+    std::cout << std::fixed << std::setprecision(8) << std::endl;
+
     try {
+        double start = retrieve_cpu_time();
         std::string encoded = encode(input, frequencies);
+        double end = retrieve_cpu_time();
+        std::cout << (long double)input.length() * ENCODING / (long double)encoded.find(']') << std::endl << end - start << std::endl;;
         output << encoded; input_binary << encoded;
     } catch (const char *err) {
         output.close();
